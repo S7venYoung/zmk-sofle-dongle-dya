c7ae850f77c562a24a326cf3795c3f4a80253cda
S7venYoung <S7venYoung@users.noreply.github.com>
2026-08-12 09:50:40 +0800

    Add editable display settings API

---
+++ include/zmk/display_settings.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2026 S7venYoung
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+bool zmk_display_settings_key_stats_enabled(void);
+int32_t zmk_display_settings_key_stats_x(void);
+int32_t zmk_display_settings_key_stats_y(void);
+int32_t zmk_display_settings_layer_alignment(void);
