7a3b9d316f89befd72603cc4e8b5e439f2b5b9b3
S7venYoung <S7venYoung@users.noreply.github.com>
2026-08-19 15:14:42 +0800

    fix(display): add YADS-style dashboard gauge ticks

--- boards/shields/dongle_display/widgets/wpm_status.h
+++ boards/shields/dongle_display/widgets/wpm_status.h
@@ -15,6 +15,7 @@ struct zmk_widget_wpm_status
     lv_obj_t *obj;
     lv_obj_t *speedometer;
     lv_obj_t *gauge_arc;
+    lv_obj_t *gauge_ticks[7];
     lv_obj_t *wpm_label;
     lv_obj_t *needle;
     lv_point_precise_t needle_points[2];
