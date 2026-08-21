cf0f593e315c9677fab0a53c4c61f4312083e850
S7venYoung <S7venYoung@users.noreply.github.com>
2026-08-21 09:23:09 +0800

    feat(display): integrate BMW brace theme

--- boards/shields/dongle_display/custom_status_screen.c
+++ boards/shields/dongle_display/custom_status_screen.c
@@ -14,6 +14,7 @@
 #include "widgets/wpm_status.h"
 #include "widgets/key_stats_status.h"
 #include "widgets/typed_keys_status.h"
+#include "widgets/bmw_theme.h"
 
 #if IS_ENABLED(CONFIG_ZMK_CUSTOM_SETTINGS)
 #include <zmk/display_settings.h>
@@ -44,6 +45,7 @@ static struct zmk_widget_bongo_cat bongo_cat_widget;
 
 static struct zmk_widget_wpm_status wpm_status_widget;
 static struct zmk_widget_typed_keys_status typed_keys_status_widget;
+static struct zmk_widget_bmw_theme bmw_theme_widget;
 
 #if IS_ENABLED(CONFIG_ZMK_KEY_STATS)
 static struct zmk_widget_key_stats_status key_stats_status_widget;
@@ -68,11 +70,20 @@ static void apply_runtime_display_settings(struct k_work *work) {
         return;
     }
 
-    bool yads_theme = zmk_display_settings_theme() == 1;
+    int32_t theme = zmk_display_settings_theme();
+    bool yads_theme = theme == 1;
+    bool bmw_theme = theme == 2;
 
-    zmk_widget_output_status_set_compact(&output_status_widget, yads_theme);
-    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget),
-                 yads_theme ? LV_ALIGN_TOP_RIGHT : LV_ALIGN_TOP_LEFT, 0, 0);
+    set_widget_visible(zmk_widget_bmw_theme_obj(&bmw_theme_widget), bmw_theme);
+
+    zmk_widget_output_status_set_compact(&output_status_widget, yads_theme || bmw_theme);
+    if (yads_theme) {
+        lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_RIGHT, 0, 0);
+    } else if (bmw_theme) {
+        lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_BOTTOM_MID, 0, 0);
+    } else {
+        lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+    }
 
     set_widget_visible(zmk_widget_typed_keys_status_obj(&typed_keys_status_widget), yads_theme);
     if (yads_theme) {
@@ -82,8 +93,8 @@ static void apply_runtime_display_settings(struct k_work *work) {
 
 #if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_WPM)
     set_widget_visible(zmk_widget_wpm_status_obj(&wpm_status_widget),
-                       yads_theme || zmk_display_settings_wpm_enabled());
-    if (yads_theme) {
+                       yads_theme || bmw_theme || zmk_display_settings_wpm_enabled());
+    if (yads_theme || bmw_theme) {
         lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
     } else {
         lv_obj_align_to(zmk_widget_wpm_status_obj(&wpm_status_widget),
@@ -95,14 +106,14 @@ static void apply_runtime_display_settings(struct k_work *work) {
 
 #if IS_ENABLED(CONFIG_ZMK_KEY_STATS)
     set_widget_visible(zmk_widget_key_stats_status_obj(&key_stats_status_widget),
-                       !yads_theme && zmk_display_settings_key_stats_enabled());
+                       !yads_theme && !bmw_theme && zmk_display_settings_key_stats_enabled());
     lv_obj_align(zmk_widget_key_stats_status_obj(&key_stats_status_widget), LV_ALIGN_TOP_LEFT,
                  MAX(0, zmk_display_settings_key_stats_x() - 4),
                  zmk_display_settings_key_stats_y());
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_BONGO_CAT)
-    bool bongo_cat_enabled = !yads_theme && zmk_display_settings_bongo_cat_enabled();
+    bool bongo_cat_enabled = !yads_theme && !bmw_theme && zmk_display_settings_bongo_cat_enabled();
     set_widget_visible(zmk_widget_bongo_cat_obj(&bongo_cat_widget), bongo_cat_enabled);
 #else
     bool bongo_cat_enabled = false;
@@ -112,9 +123,11 @@ static void apply_runtime_display_settings(struct k_work *work) {
     set_widget_visible(zmk_widget_modifiers_obj(&modifiers_widget),
                        zmk_display_settings_modifiers_enabled());
     zmk_widget_modifiers_set_active_only(&modifiers_widget, yads_theme);
-    lv_obj_align(zmk_widget_modifiers_obj(&modifiers_widget),
-                 yads_theme ? LV_ALIGN_CENTER : LV_ALIGN_BOTTOM_LEFT, 0,
-                 yads_theme ? 8 : 0);
+    if (yads_theme) {
+        lv_obj_align(zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_CENTER, 0, 8);
+    } else {
+        lv_obj_align(zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
+    }
     zmk_widget_modifiers_refresh(&modifiers_widget);
 #if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
     set_widget_visible(zmk_widget_hid_indicators_obj(&hid_indicators_widget),
@@ -126,6 +139,10 @@ static void apply_runtime_display_settings(struct k_work *work) {
     set_widget_visible(zmk_widget_layer_status_obj(&layer_status_widget),
                        !yads_theme && zmk_display_settings_layer_enabled());
     zmk_widget_layer_status_refresh(&layer_status_widget);
+    if (bmw_theme) {
+        lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_RIGHT, 0,
+                     -3);
+    } else
 #if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_BONGO_CAT)
     if (bongo_cat_enabled) {
         lv_obj_align_to(zmk_widget_layer_status_obj(&layer_status_widget),
@@ -169,6 +186,10 @@ lv_obj_t *zmk_display_status_screen() {
     lv_style_set_text_letter_space(&global_style, 1);
     lv_style_set_text_line_space(&global_style, 1);
     lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
+
+    zmk_widget_bmw_theme_init(&bmw_theme_widget, screen);
+    lv_obj_align(zmk_widget_bmw_theme_obj(&bmw_theme_widget), LV_ALIGN_CENTER, 0, 0);
+    set_widget_visible(zmk_widget_bmw_theme_obj(&bmw_theme_widget), false);
     
     zmk_widget_output_status_init(&output_status_widget, screen);
     lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
