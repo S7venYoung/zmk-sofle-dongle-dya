6ed8dcdc4f5151f9bf37e84d5fb8aa2e177953ed
S7venYoung <S7venYoung@users.noreply.github.com>
2026-08-21 17:31:10 +0800

    Center BMW needles and align their scale

--- boards/shields/dongle_display/widgets/wpm_status.c
+++ boards/shields/dongle_display/widgets/wpm_status.c
@@ -53,9 +53,9 @@ static struct wpm_status_state get_state(const zmk_event_t *eh) {
 
 static lv_point_precise_t brace_target(int value, bool right) {
     int level = CLAMP(value, 0, BMW_WPM_MAX);
-    lv_point_precise_t low = {22, 41};
-    lv_point_precise_t middle = {42, 27};
-    lv_point_precise_t high = {22, 5};
+    lv_point_precise_t low = {22, 40};
+    lv_point_precise_t middle = {42, 24};
+    lv_point_precise_t high = {22, 6};
     lv_point_precise_t target;
 
     if (level <= BMW_WPM_MAX / 2) {
@@ -127,14 +127,14 @@ int zmk_widget_wpm_status_init(struct zmk_widget_wpm_status *widget, lv_obj_t *p
     widget->current_value = create_value_label(widget->obj, 5);
     widget->peak_value = create_value_label(widget->obj, 103);
 
-    widget->current_points[0] = (lv_point_precise_t){31, 34};
-    widget->current_points[1] = (lv_point_precise_t){22, 41};
+    widget->current_points[0] = (lv_point_precise_t){23, 24};
+    widget->current_points[1] = (lv_point_precise_t){22, 40};
     widget->current_needle = lv_line_create(widget->obj);
     lv_line_set_points(widget->current_needle, widget->current_points, 2);
     lv_obj_set_style_line_width(widget->current_needle, 1, 0);
 
-    widget->peak_points[0] = (lv_point_precise_t){96, 34};
-    widget->peak_points[1] = (lv_point_precise_t){105, 41};
+    widget->peak_points[0] = (lv_point_precise_t){104, 24};
+    widget->peak_points[1] = (lv_point_precise_t){105, 40};
     widget->peak_needle = lv_line_create(widget->obj);
     lv_line_set_points(widget->peak_needle, widget->peak_points, 2);
     lv_obj_set_style_line_width(widget->peak_needle, 1, 0);
