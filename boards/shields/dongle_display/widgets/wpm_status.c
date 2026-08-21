5055803bd32f23fb434812145a9af90c5eae2ae2
S7venYoung <S7venYoung@users.noreply.github.com>
2026-08-21 17:19:23 +0800

    Show live and peak values inside BMW gauges

--- boards/shields/dongle_display/widgets/wpm_status.c
+++ boards/shields/dongle_display/widgets/wpm_status.c
@@ -53,9 +53,9 @@ static struct wpm_status_state get_state(const zmk_event_t *eh) {
 
 static lv_point_precise_t brace_target(int value, bool right) {
     int level = CLAMP(value, 0, BMW_WPM_MAX);
-    lv_point_precise_t low = {20, 41};
-    lv_point_precise_t middle = {10, 24};
-    lv_point_precise_t high = {42, 1};
+    lv_point_precise_t low = {22, 41};
+    lv_point_precise_t middle = {42, 27};
+    lv_point_precise_t high = {22, 5};
     lv_point_precise_t target;
 
     if (level <= BMW_WPM_MAX / 2) {
@@ -76,12 +76,14 @@ static lv_point_precise_t brace_target(int value, bool right) {
 static void set_wpm(struct zmk_widget_wpm_status *widget, struct wpm_status_state state) {
     if (state.current != widget->last_current) {
         widget->last_current = state.current;
+        lv_label_set_text_fmt(widget->current_value, "%d", state.current);
         widget->current_points[1] = brace_target(state.current, false);
         lv_line_set_points(widget->current_needle, widget->current_points, 2);
     }
 
     if (state.peak != widget->last_peak) {
         widget->last_peak = state.peak;
+        lv_label_set_text_fmt(widget->peak_value, "%d", state.peak);
         widget->peak_points[1] = brace_target(state.peak, true);
         lv_line_set_points(widget->peak_needle, widget->peak_points, 2);
     }
@@ -104,6 +106,15 @@ void zmk_widget_wpm_status_refresh(struct zmk_widget_wpm_status *widget) {
     set_wpm(widget, get_state(NULL));
 }
 
+static lv_obj_t *create_value_label(lv_obj_t *parent, lv_coord_t x) {
+    lv_obj_t *label = lv_label_create(parent);
+    lv_obj_set_pos(label, x, 20);
+    lv_obj_set_width(label, 20);
+    lv_obj_set_style_text_font(label, &lv_font_unscii_8, 0);
+    lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, 0);
+    return label;
+}
+
 int zmk_widget_wpm_status_init(struct zmk_widget_wpm_status *widget, lv_obj_t *parent) {
     widget->obj = lv_obj_create(parent);
     lv_obj_remove_style_all(widget->obj);
@@ -113,14 +124,17 @@ int zmk_widget_wpm_status_init(struct zmk_widget_wpm_status *widget, lv_obj_t *p
     lv_img_set_src(widget->background, &bmw_brace_image);
     lv_obj_align(widget->background, LV_ALIGN_TOP_LEFT, 0, 0);
 
+    widget->current_value = create_value_label(widget->obj, 5);
+    widget->peak_value = create_value_label(widget->obj, 103);
+
     widget->current_points[0] = (lv_point_precise_t){31, 34};
-    widget->current_points[1] = (lv_point_precise_t){20, 41};
+    widget->current_points[1] = (lv_point_precise_t){22, 41};
     widget->current_needle = lv_line_create(widget->obj);
     lv_line_set_points(widget->current_needle, widget->current_points, 2);
     lv_obj_set_style_line_width(widget->current_needle, 1, 0);
 
     widget->peak_points[0] = (lv_point_precise_t){96, 34};
-    widget->peak_points[1] = (lv_point_precise_t){107, 41};
+    widget->peak_points[1] = (lv_point_precise_t){105, 41};
     widget->peak_needle = lv_line_create(widget->obj);
     lv_line_set_points(widget->peak_needle, widget->peak_points, 2);
     lv_obj_set_style_line_width(widget->peak_needle, 1, 0);
@@ -129,6 +143,7 @@ int zmk_widget_wpm_status_init(struct zmk_widget_wpm_status *widget, lv_obj_t *p
     widget->last_peak = -1;
     sys_slist_append(&widgets, &widget->node);
     widget_wpm_status_init();
+    zmk_widget_wpm_status_refresh(widget);
     return 0;
 }
 
