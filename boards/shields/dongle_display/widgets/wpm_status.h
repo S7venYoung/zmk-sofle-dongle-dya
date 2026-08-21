343201d314fb3b8cbb8d6576a301dfc0cfbb2f3b
S7venYoung <S7venYoung@users.noreply.github.com>
2026-08-21 10:10:43 +0800

    refactor(display): reduce BMW WPM widget state

--- boards/shields/dongle_display/widgets/wpm_status.h
+++ boards/shields/dongle_display/widgets/wpm_status.h
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2024 The ZMK Contributors
- *
+ * Copyright (c) 2026 S7venYoung
  * SPDX-License-Identifier: MIT
  */
 
@@ -9,31 +8,18 @@
 #include <lvgl.h>
 #include <zephyr/kernel.h>
 
-struct zmk_widget_wpm_status
-{
+struct zmk_widget_wpm_status {
     sys_snode_t node;
     lv_obj_t *obj;
-    lv_obj_t *speedometer;
-    lv_obj_t *gauge_arc;
-    lv_obj_t *gauge_ticks[7];
-    lv_obj_t *wpm_label;
-    lv_obj_t *needle;
-    lv_point_precise_t needle_points[2];
-    lv_obj_t *bmw_outline;
-    lv_point_precise_t bmw_outline_points[3];
-    lv_obj_t *bmw_ticks[6];
-    lv_point_precise_t bmw_tick_points[6][2];
-    lv_obj_t *bmw_needle;
-    lv_point_precise_t bmw_needle_points[2];
-    uint8_t display_mode;
-    bool peak_mode;
-    int last_value;
+    lv_obj_t *background;
+    lv_obj_t *current_needle;
+    lv_obj_t *peak_needle;
+    lv_point_precise_t current_points[2];
+    lv_point_precise_t peak_points[2];
+    int last_current;
+    int last_peak;
 };
 
 int zmk_widget_wpm_status_init(struct zmk_widget_wpm_status *widget, lv_obj_t *parent);
 lv_obj_t *zmk_widget_wpm_status_obj(struct zmk_widget_wpm_status *widget);
 void zmk_widget_wpm_status_refresh(struct zmk_widget_wpm_status *widget);
-void zmk_widget_wpm_status_set_peak(struct zmk_widget_wpm_status *widget, bool peak);
-void zmk_widget_wpm_status_set_dashboard(struct zmk_widget_wpm_status *widget, bool enabled);
-void zmk_widget_wpm_status_set_bmw(struct zmk_widget_wpm_status *widget, bool enabled,
-                                   bool right_side);
